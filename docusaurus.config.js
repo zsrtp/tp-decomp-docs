6431626bb2472ce2fe39570f293ce08f647a598e
TakaRikka <takarikka@outlook.com>
2023-06-19 18:03:16 -0700

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,108 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+
+const REPO_URL = 'https://github.com/zeldaret/tp';
+const DISCORD_URL = 'https://discord.zelda64.dev';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Twilight Princess Decompilation',
+  tagline: '--tagline--',
+  url: 'https://zsrtp.github.io',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  trailingSlash: false,
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  baseUrlIssueBanner: false,
+
+  plugins: [require.resolve('@cmfcmf/docusaurus-search-local')],
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+          blogSidebarCount: 0,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Twilight Princess Decompilation',
+        logo: {
+          alt: 'Twilight Princess Decompilation',
+          src: 'img/logo.png',
+          height: 32,
+          width: 32,
+        },
+        items: [
+          {
+            type: 'doc',
+            position: 'left',
+            label: 'About',
+            docId: 'about',
+          },
+          { to: '/progress', label: 'Progress', position: 'left' },
+          {
+            type: 'doc',
+            position: 'left',
+            label: 'Contribute',
+            docId: 'contribute/index',
+          },
+          {
+            href: REPO_URL,
+            label: 'GitHub',
+            position: 'left',
+          },
+          {
+            type: 'search',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+              { label: 'Decomp GitHub', href: REPO_URL },
+              { label: 'Website GitHub', href: 'https://github.com/zsrtp/tp-decomp-docs' },
+              { label: 'ZeldaRET', href: 'https://zelda64.dev' },
+              { label: 'ZeldaRET Discord', href: DISCORD_URL },
+        ],
+      },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
+      prism: {
+        darkTheme: darkCodeTheme,
+        defaultLanguage: 'cpp',
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import clsx from 'clsx';
+
+import Head from '@docusaurus/Head';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+import styles from './index.module.css';
+
+function Block({ title, children }) {
+  return (
+    <div className={clsx("col", styles.card)}>
+      <h2 className="text-hilight">{title}</h2>
+      {children}
+    </div>
+  );
+}
+
+export default function Index() {
+  const ctx = useDocusaurusContext();
+
+  return (
+    <Layout description={ctx.siteConfig.tagline}>
+      <Head>
+        <html className={clsx("container-tp-background")} id={styles.page} />
+      </Head>
+      <header className={styles.heroBanner}>
+        <div className="container">
+          <h2 className="hero__title">Twilight Princess Decomp</h2>
+
+          <div className="row">
+            <Block title="What is this?">
+              <p>This is a reverse engineering project to decompile <i className="text-hilight">Twilight Princess</i> into human-readable and modifiable source code.</p>
+
+              <Link
+                className="button button--secondary"
+                to="/about">
+                Learn more
+              </Link>
+            </Block>
+
+            <Block title="I want to help">
+              <p>We gladly accept contributions! This is a group effort and every contribution helps.</p>
+
+              <Link
+                className="button button--secondary"
+                to="/contribute">
+                Get started
+              </Link>
+            </Block>
+          </div>
+        </div>
+      </header>
+    </Layout>
+  );
+}
