bd4d62715a263a85f8092d83827ca1a040e72540
Trueffeloot <geiloking135@gmail.com>
2023-06-22 05:01:24 +0200

    Progress graph, decompiler-setup page, wallpaper

--- src/pages/progress.tsx
+++ src/pages/progress.tsx
@@ -1,14 +1,8 @@
 import '@site/src/chartGlobals';
 
-import React, {
-  useEffect,
-  useState,
-} from 'react';
-
-import { ChartOptions } from 'chart.js/auto';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { Chart } from 'react-chartjs-2';
-
 import Head from '@docusaurus/Head';
 import * as progressUtils from '@site/src/progressUtils';
 import Layout from '@theme/Layout';
@@ -18,32 +12,30 @@ import styles from './progress.module.css';
 function InfoRow({ title, value, secondLevel = false }) {
   return (
     <p className={clsx(styles.infoRow, secondLevel && styles.secondLevel)}>
-      <span className="key">{secondLevel && "- "}{title}</span>{" "}
+      <span className="key">{secondLevel && '- '}{title}</span>{' '}
       <span className="value">{value}</span>
     </p>
   );
 }
 
-function formatPercent(x: number, digits = 3) {
-  return (100 * x).toFixed(digits) + "%";
+function formatPercent(x, digits = 3) {
+  return (100 * x).toFixed(digits) + '%';
 }
 
-function formatProgress(category: progressUtils.Counts, total: progressUtils.Counts) {
-  return `${formatPercent(category.size / total.size)}`;
+function formatProgress(category, total) {
+  return formatPercent(category / total);
 }
 
-function formatSizeAsMb(sizeBytes: number, digits = 3) {
+function formatSizeAsMb(sizeBytes, digits = 3) {
   return (sizeBytes / 1_000_000).toFixed(digits);
 }
 
 export default function Progress() {
-  const [functions, setFunctions] = useState("?????/?????? - ??.???%");
-  const [decompiled, setDecompiled] = useState("?.???/??.??? MB - ??.???%");
-  const [matching, setMatching] = useState("??.???%");
-  const [nmMinor, setNmMinor] = useState("?.???%");
-  const [nmMajor, setNmMajor] = useState("?.???%");
+  const [totalCode, setTotalCode] = useState('?.???/??.??? MB - ??.???%');
+  const [dol, setDol] = useState('?.???/??.??? MB - ??.???%');
+  const [rels, setRels] = useState('?.???/??.??? MB - ??.???%');
 
-  const [entries, setEntries] = useState<progressUtils.Entry[]>([]);
+  const [entries, setEntries] = useState([]);
   const [chartData, setChartData] = useState(null);
 
   useEffect(() => {
@@ -53,27 +45,23 @@ export default function Progress() {
 
       const last = entries[entries.length - 1];
 
-      setFunctions(`${last.decompiled.count}/${last.total.count} - ${formatPercent(last.decompiled.count / last.total.count)}`);
-      setDecompiled(`${formatSizeAsMb(last.decompiled.size)}/${formatSizeAsMb(last.total.size)} MB - ${formatProgress(last.decompiled, last.total)}`);
-      setMatching(`${formatProgress(last.matching, last.total)}`);
-      setNmMinor(`${formatProgress(last.nmMinor, last.total)}`);
-      setNmMajor(`${formatProgress(last.nmMajor, last.total)}`);
+      setTotalCode(`${formatSizeAsMb(last.total_code.count)}/${formatSizeAsMb(last.total_code.size)} MB - ${formatProgress(last.total_code.count, last.total_code.size)}`);
+      setDol(`${formatSizeAsMb(last.dol.count)}/${formatSizeAsMb(last.dol.size)} MB - ${formatProgress(last.dol.count, last.dol.size)}`);
+      setRels(`${formatSizeAsMb(last.rels.count)}/${formatSizeAsMb(last.rels.size)} MB - ${formatProgress(last.rels.count, last.rels.size)}`);
 
       setChartData({
         datasets: [
           {
-            label: "Decompiled size%",
-            data: entries.map(entry => {
-              return {
-                x: entry.time,
-                y: entry.decompiled.size / entry.total.size,
-              };
-            }),
+            label: 'Total code %',
+            data: entries.map(entry => ({
+              x: entry.time,
+              y: entry.total_code.count / entry.total_code.size,
+            })),
             pointRadius: 0,
             pointHoverRadius: 0,
-            pointBackgroundColor: "#20adff",
-            borderColor: "#20adff",
-            cubicInterpolationMode: "monotone",
+            pointBackgroundColor: '#f1c40f',
+            borderColor: '#f1c40f',
+            cubicInterpolationMode: 'monotone',
           },
         ],
       });
@@ -82,22 +70,22 @@ export default function Progress() {
     load();
   }, []);
 
-  const chartOptions: ChartOptions = {
+  const chartOptions = {
     maintainAspectRatio: false,
     scales: {
       x: {
-        type: "time",
-        title: { display: false, text: "Date" },
+        type: 'time',
+        title: { display: false, text: 'Date' },
         ticks: {
           autoSkip: true,
           maxTicksLimit: 12,
         },
       },
       y: {
-        title: { display: true, text: "Decompiled %" },
+        title: { display: true, text: 'Decompiled %' },
         beginAtZero: true,
         ticks: {
-          callback: (value: number, _index: any, _values: any) => formatPercent(value, 2),
+          callback: (value, _index, _values) => formatPercent(value, 2),
         },
       },
     },
@@ -109,52 +97,48 @@ export default function Progress() {
         display: false,
       },
       tooltip: {
-        mode: "index",
+        mode: 'index',
         intersect: false,
-        position: "nearest",
+        position: 'nearest',
         callbacks: {
           label: (item) => {
             const entry = entries[item.dataIndex];
             const lines = [];
-            lines.push("Decompiled %: " + formatProgress(entry.decompiled, entry.total));
-            if (item.datasetIndex == 1)
-              return lines;
-            lines.push(`Decompiled size: ${entry.decompiled.size} bytes`);
-            lines.push("Matching %: " + formatProgress(entry.matching, entry.total));
-            lines.push("Commit: " + entry.rev);
+            lines.push('Total Code %: ' + formatProgress(entry.total_code.count, entry.total_code.size));
+            if (item.datasetIndex === 1) return lines;
+            lines.push('main.dol: ' + formatProgress(entry.dol.count, entry.dol.size));
+            lines.push('RELs: ' + formatProgress(entry.rels.count, entry.rels.size));
+            lines.push('Commit: ' + entry.rev);
             return lines;
           },
         },
         displayColors: false,
-        borderColor: "#fff",
+        borderColor: '#fff',
         borderWidth: 1,
       },
     },
   };
 
   return (
-    <Layout noFooter title="Progress" description="Current progress of the Breath of the Wild decompilation project">
+    <Layout noFooter title="Progress" description="Current progress of the Twilight Princess decompilation project">
       <Head>
-        <html className={clsx("container-tp-background")} id={styles.page} />
+        <html className={clsx('container-tp-background')} id={styles.page} />
       </Head>
       <div className={styles.container}>
         <h1>Decompilation Progress</h1>
-        <InfoRow title="Functions" value={functions} />
-        <InfoRow title="Decompiled" value={decompiled} />
-        <InfoRow title="Matching" value={matching} secondLevel />
-        <InfoRow title="Non-matching (minor issues)" value={nmMinor} secondLevel />
-        <InfoRow title="Non-matching (major issues)" value={nmMajor} secondLevel />
-        <div
-          className="chart-container"
-          style={{ marginTop: 30, minHeight: 300 }}
-        >
-          {chartData && <Chart
-            aria-label="Decompilation progress chart"
-            role="img"
-            type="line"
-            data={chartData}
-            options={chartOptions}
-          />}
+        <InfoRow title="Total Code" value={totalCode} />
+        <InfoRow title="main.dol" value={dol} />
+        <InfoRow title="RELs" value={rels} />
+        <div className="chart-container" style={{ marginTop: 30, minHeight: 300 }}>
+          {chartData && (
+            <Chart
+              aria-label="Decompilation progress chart"
+              role="img"
+              type="line"
+              data={chartData}
+              options={chartOptions}
+            />
+          )}
         </div>
       </div>
     </Layout>
